384d933ce644ef20687ad6aa33c01468509e349a
Chernyshev.V.D <Chernyshev.V.D.@gmail.com>
2020-12-16 02:33:12 +0300

    corrected task3_1

--- task3_1.py
+++ task3_1.py
@@ -3,17 +3,30 @@
 их деление. Числа запрашивать у пользователя.
 Предусмотреть обработку ситуации деления на ноль.
 """
-user_var1 = int(input("Введите первое число\n>>>"))
-user_var2 = int(input("Введите второе число\n>>>"))
 
-def division(user_var1, user_var2):
+while True:
+    user_var1 = input("Введите первое число\n>>>")
+    try:
+        user_var1 = int(user_var1)
+    except ValueError as e:
+        print(f'{e}\nЗаданы некорректные условия.\nНеверное значение данных.')
+        continue
+    break
+
+while True:
+    user_var2 = input("Введите второе число\n>>>")
+    try:
+        user_var2 = int(user_var2)
+    except ValueError as e:
+        print(f'{e}\nЗаданы некорректные условия.\nНеверное значение данных.')
+        continue
     if user_var2 == 0:
         print('Заданы некорректные условия. \nНа ноль делить нельзя.')
-        exit()
-    else:
-        result = user_var1 / user_var2
+        continue
+    break
+
+def division(user_var1, user_var2):
+    result = user_var1 / user_var2
     return result
 
 print(division(user_var1, user_var2))
-
-
