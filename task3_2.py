2e9b4832d71a865469a1dc19dea7a9bef476f0c9
Chernyshev.V.D <Chernyshev.V.D.@gmail.com>
2020-12-17 03:15:13 +0300

    corrected task3_2.py

--- task3_2.py
+++ task3_2.py
@@ -5,3 +5,51 @@
 Реализовать вывод данных о пользователе одной строкой.
 """
 
+var_template = {
+    'Name': ('Укажите Ваше имя:', str),
+    'Surname': ('Укажите Вашу фамилию:', str),
+    'Birthday': ('Ваш год рождения (необходимо указать число в формате XXXX)', int),
+    'City': ('Город проживания:', str),
+    'Email': ('Электронная почта:', str),
+    'Telephone': ('Телефон:', int),
+}
+'''
+
+def user_worksheet(var_template):
+
+    user  = {}
+    for key, value in var_template.items():
+        while True:
+            user_value = input(f'{value[0]}\n>>>')
+            try:
+                user_value = value[1](user_value)
+            except ValueError as e:
+                print(f'{e}\nНеверно указаны данные.')
+                continue
+            user[key] = user_value
+            break
+    return user
+
+print('#'*40, '\n', user_worksheet(var_template))
+
+'''
+
+def user_worksheet(*user):
+
+    user  = {}
+    for key, value in var_template.items():
+        while True:
+            user_value = input(f'{value[0]}\n>>>')
+            try:
+                user_value = value[1](user_value)
+            except ValueError as e:
+                print(f'{e}\nНеверно указаны данные.')
+                continue
+            user[key] = user_value
+            break
+    return user
+user_dict = user_worksheet(var_template)
+print('#'*40, f'\n{user_dict}\n', '#'*40)
+print(f"Имя: {user_dict.get('Name')}; Фамилия: {user_dict.get('Surname')}; Год рождения: {user_dict.get('Birthday')}; Город: {user_dict.get('City')}; Email: {user_dict.get('Email')}; Тел.: {user_dict.get('Telephone')}.", '\n', '#'*40)
+
+
