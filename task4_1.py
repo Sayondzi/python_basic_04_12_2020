4954507a0f6199bd9993e73d01ced195edeba8f1
Chernyshev.V.D <Chernyshev.V.D.@gmail.com>
2020-12-20 14:09:24 +0300

    corrected task4_1.py

--- task4_1.py
+++ task4_1.py
@@ -6,3 +6,22 @@
 Для выполнения расчета для конкретных значений необходимо запускать скрипт с
 параметрами.
 '''
+
+from sys import argv
+from task4_1a import my_wages
+
+if len(argv) != 4:
+    print('Ошибка ввода параметров.')
+    exit()
+
+variables = []
+for param in argv[1:]:
+    try:
+        param = float(param)
+        variables.append(param)
+    except ValueError as e:
+        print(f'{e}\nВведены неверные значения.')
+        exit()
+        break
+
+print(my_wages(*variables))
