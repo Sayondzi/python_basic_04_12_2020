4b929bf3ee2dceb6c39fccf0a3a650dea6af1999
Chernyshev.V.D <Chernyshev.V.D.@gmail.com>
2020-12-29 04:40:57 +0300

    Add task6_01a.py, task6_01b.py, task6_02.py, task6_03.py, task6_04.py, task6_05.py.

---
+++ task6_03.py
@@ -0,0 +1,44 @@
+'''
+Реализовать базовый класс Worker (работник), в котором определить атрибуты:
+name, surname, position (должность), income (доход).
+Последний атрибут должен быть защищенным и ссылаться на словарь, содержащий элементы: оклад и премия,
+например, {"wage": wage, "bonus": bonus}.
+Создать класс Position (должность) на базе класса Worker.
+В классе Position реализовать методы получения полного имени сотрудника (get_full_name) и дохода с учетом
+премии (get_total_income).
+Проверить работу примера на реальных данных (создать экземпляры класса Position, передать данные,
+проверить значения атрибутов, вызвать методы экземпляров).
+'''
+
+class Worker:
+    def __init__(self, name: str, surname: str, position: str):
+        '''
+        :param name: имя
+        :param surname: фамилия
+        :param position: должность
+        '''
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": 0, "bonus": 0}
+
+class Position(Worker):
+
+    def __init__(self, name, surname, wage: float, bonus: float):
+
+        '''
+        :param name: имя
+        :param surname: фамилия
+        :param wage: оклад
+        :param bonus: премия
+        '''
+
+        super().__init__(name, surname)
+        self._income["wage"] = wage
+        self._income["bonus"] = bonus
+
+    def get_full_name(self):
+        return f'{self.name} {self.surname}'
+
+    def get_total_income(self):
+        return sum(self._income.values())
